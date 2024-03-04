3a12d31cdceee3e030b2cb0c2b943f09e2000b41
Sir Abubakar <60545690+Abubakar-Abdulwahab@users.noreply.github.com>
2024-03-04 17:28:08 +0100

    updates

--- src/services/proxyLogin.ts
+++ src/services/proxyLogin.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const baseURL = 'http://24.144.116.26:3000';
+// const baseURL = 'http://24.144.116.26:3000';
+const baseURL = 'http://198.199.84.133:3000';
 
 export const proxyLogin = async (email: string, password: string) => {
   const res = axios.post(baseURL + '/auth/login', { email, password });
